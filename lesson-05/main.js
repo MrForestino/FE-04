56239bd42b4ff6e479bf7bb45c4453015d533b6d
MrForestino <borv2006@gmail.com>
2025-06-16 18:36:28 +0200

    update

---
+++ lesson-05/main.js
@@ -0,0 +1,6 @@
+const keys = ["q","w","e", "r", "t", "y", "1","2","3","`"];
+const key = document.querySelector(".show");
+const gameBtn = document.querySelector(".startBtn");
+
+let isActive;
+let  currentKeyIndex = 0;
