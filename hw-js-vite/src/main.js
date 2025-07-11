52757d2c062dffa7cbc718698b7af25101294cec
MrForestino <borv2006@gmail.com>
2025-07-11 18:56:00 +0200

    json file

--- hw-js-vite/src/main.js
+++ hw-js-vite/src/main.js
@@ -1,24 +1,29 @@
 import './style.css'
-import javascriptLogo from './javascript.svg';
-import viteLogo from '../public/vite.svg'
-import { setupCounter } from './counter.js'
+import studentsJSON from './json/students.json';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let dataArray = [];
+let dataJSON = '';
 
-setupCounter(document.querySelector('#counter'))
+dataArray = studentsJSON;
+
+console.log("dataArray:", dataArray)
+
+localStorage.setItem('students', JSON.stringify(dataArray))
+
+const dataArrayLocalStorage = localStorage.getItem('students', dataArray)
+
+console.log("dataArrayLocalStorage: ", dataArrayLocalStorage)
+
+const studentsList = document.getElementById('students-list')
+
+studentsList.innerHTML = dataArray.map(student => {
+  return `
+    <li class="studentItem">
+      <h3>Ім'я: ${student.firstName}</h3>
+      <h3>Прізвище: ${student.lastName}</h3>
+      <p>Вік: ${student.age}</p>
+      <p>Курс: ${student.course}</p>
+      <p>Факльтет: ${student.faculty}</p>
+    </li>
+    `
+}).join('');
